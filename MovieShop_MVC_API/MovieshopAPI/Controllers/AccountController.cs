0bec9e50fa6a25d9034e01f18515fe068a7e59ca
HaibaraAiChan <sophiehexyang@gmail.com>
2025-10-16 15:03:59 -0700

    add API

---
+++ MovieShop_MVC_API/MovieshopAPI/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Infrastructure.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ApplicationCore.Models;
+
+namespace MovieshopAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly AccountService _accountService;
+        public AccountController(AccountService accountService)
+        {
+            _accountService = accountService;
+        }
+        
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login( LoginModel model)
+        {
+            
+            var user = await _accountService.ValidateUser(model.Email, model.Password);
+            //if (user == null)
+            //{
+            //    return Unauthorized(new { errorMessage = "Invalid email or password" });
+            //}
+            return Ok(user);
+        }
+
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register( RegisterModel model)
+        {
+            
+            var user = await _accountService.RegisterUser(model);
+            //if (user == null)
+            //{
+            //    return BadRequest(new { errorMessage = "Registration failed" });
+            //}
+            return Ok(user);
+        }
+    }
+}
